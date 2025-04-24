af3d20ba3a51d8fd0825489b3a656e4fa0b31e75
yeji-star <sizuku134@gmail.com>
2025-04-24 11:16:35 +0900

    mysql이랑 연동

---
+++ src/main/java/com/KoreaIT/java/AM_jsp/articleListServlet.java
@@ -0,0 +1,70 @@
+package com.KoreaIT.java.AM_jsp;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/article/list")
+public class articleListServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+
+		// DB 연결
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("클래스 x");
+			e.printStackTrace();
+
+		}
+
+		response.getWriter().append("123");
+
+		String url = "jdbc:mysql://127.0.0.1:3306/AM_DB_25_03?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul";
+		String user = "root";
+		String password = "";
+
+		Connection conn = null;
+
+		try {
+			conn = DriverManager.getConnection(url, user, password);
+			response.getWriter().append("연결 성공!");
+
+			DBUtil dbUtil = new DBUtil(request, response);
+
+			String sql = "SELECT * FROM article;";
+
+			List<Map<String, Object>> articleRows = dbUtil.selectRows(conn, sql); // DB에서 가져온 걸 압축풀기한 느낌
+			
+			request.setAttribute("articleRows", articleRows); // 속성에 대해서 하나를 설명
+			
+//			response.getWriter().append(articleRows.toString()); // 출력하는 놈(데이터);
+			
+			request.getRequestDispatcher("/jsp/article/list.jsp").forward(request, response); //연동
+
+		} catch (SQLException e) {
+			System.out.println("에러 1 : " + e);
+		} finally {
+			try {
+				if (conn != null && !conn.isClosed()) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
--- src/main/webapp/jsp/home/printDan.jsp
+++ src/main/webapp/jsp/home/printDan.jsp
@@ -1,18 +1,41 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
+
+<%
+int dan = Integer.parseInt(request.getParameter("dan"));
+int limit = Integer.parseInt(request.getParameter("limit"));
+String color = request.getParameter("color");
+%>
+
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>구구단 출력</title>
+<style>
+.color {
+	color: <%=color%>
+}
+</style>
 </head>
-<body>
+<body class="color">
 	<h1>구구단 출력</h1>
 
-	<h2>8단</h2>
-	<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-		Dolorem eaque unde labore quidem totam consectetur a excepturi culpa
-		laboriosam sed expedita illo aut dolores dolore tenetur veritatis
-		aliquam! Blanditiis voluptas!</div>
+	<h2>
+		==<%=dan%>단==
+	</h2>
+	<div>
+	<%
+	for (int i = 1; i <= limit; i++) {
+	%>
+	<div><%=dan%>
+		*
+		<%=i%>
+		=
+		<%=dan * i%></div>
+	<%
+	}
+	%>
+	</div>
 </body>
 </html>
